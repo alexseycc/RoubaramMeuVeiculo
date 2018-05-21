d5255b9d087c43ec7d650f5ad7686a308395f9e5
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-05-20 21:03:28 -0300

    alexsey reboucas,student from estacio RJ

--- py/veiculo.py
+++ py/veiculo.py
@@ -17,4 +17,39 @@ class veiculo:
     self.cor=cor
   
   
-  def veiculo()
+  def veiculo(self,tipo,marca,nome,placa,cor):
+    self.tipo=tipo
+    self.marca=marca
+    self.nome=nome
+    self.placa=placa
+    self.cor=cor
+    
+  def setTipo(self,tipo):
+    self.tipo
+    
+  def getTipo(self):
+    return self.tipo
+  
+  def setMarca(self,marca):
+    self.marca=marca
+    
+  def getMarca(self):
+    return self.marca
+  
+  def setNome(self,nome):
+    self.nome=nome
+    
+  def getNome(self):
+    return self.nome
+  
+  def setPlaca(self,placa):
+    self.placa=placa
+    
+  def getPlaca(self):
+    return self.placa
+  
+  def setCor(self,cor):
+    self.cor=cor
+    
+  def getCor(self):
+    return self.cor
